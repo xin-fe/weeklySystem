ea921b88dff6b5eeda269c6778b3838e2eb6c165
unknown <tongshuo@xin.com>
2017-06-28 17:37:17 +0800

    test

--- app.js
+++ app.js
@@ -1,47 +1,39 @@
-'use strict'
-
 const Koa = require('koa')
+const app = new Koa()
 const views = require('koa-views')
-const co = require('co')
-const convert = require('koa-convert')
 const json = require('koa-json')
 const onerror = require('koa-onerror')
-const bodyparser = require('koa-bodyparser')()
+const bodyparser = require('koa-bodyparser')
 const logger = require('koa-logger')
-const ejs = require('ejs')
-const app = new Koa();
 
+const index = require('./routes/index')
+const users = require('./routes/users')
 
+// error handler
+onerror(app)
 
-const router = require('./routes/index')
+// middlewares
+app.use(bodyparser({
+  enableTypes:['json', 'form', 'text']
+}))
+app.use(json())
+app.use(logger())
+app.use(require('koa-static')(__dirname + '/public'))
 
-
-app.use(convert(bodyparser))
-app.use(convert(json()))
-app.use(convert(logger()))
-app.use(require('koa-static')(__dirname+'/public'))
-
-app.use(views(__dirname+'/views', {
-	map:{html:'ejs'}
+app.use(views(__dirname + '/views', {
+  extension: 'pug'
 }))
 
-//log
-app.use(function* (ctx, next) {
-  const start = new Date();
-  yield next()
-  const ms = new Date() - start;
+// logger
+app.use(async (ctx, next) => {
+  const start = new Date()
+  await next()
+  const ms = new Date() - start
   console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
-});
-
-
-app.use(router.routes(), router.allowedMethods())
-
-
-app.on('error', function(err, ctx){
-  console.log(err)
-  logger.error('server error', err, ctx)
 })
 
+// routes
+app.use(index.routes(), index.allowedMethods())
+app.use(users.routes(), users.allowedMethods())
 
-app.listen('3000')
-console.log('the server running at 3000')
+module.exports = app
